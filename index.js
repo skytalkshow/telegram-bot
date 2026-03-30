b1adfa452c3a10809e346d61e503df27e4699aae
skytalkshow <166543117+skytalkshow@users.noreply.github.com>
2026-03-30 14:14:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,16 @@ bot.on('message', async (msg) => {
     } catch (error) {
         bot.sendMessage(chatId, "Lỗi rồi 😢");
     }
+    
+});
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
